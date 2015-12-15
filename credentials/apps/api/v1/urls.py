4dcb7c1b2a4e027fae6f16f2e45dac4ec740c0c1
Awais <awais.qureshi@arbisoft.com>
2015-12-15 17:35:22 +0500

    APIs added for User Credentials, programs certificates and course certificates ECOM-2999

--- credentials/apps/api/v1/urls.py
+++ credentials/apps/api/v1/urls.py
@@ -1,3 +1,12 @@
 """ API v1 URLs. """
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from credentials.apps.api.v1 import views
+
+
+router = DefaultRouter()  # pylint: disable=invalid-name
+router.register(r'user_credentials', views.UserCredentialViewSet)
+router.register(r'program_credentials', views.CredentialsByProgramsViewSet)
+router.register(r'course_credentials', views.CredentialsByCoursesViewSet)
+
+urlpatterns = router.urls
